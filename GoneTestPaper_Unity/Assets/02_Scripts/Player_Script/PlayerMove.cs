a3d4d0e91877c134c706cb272d770b5af15b385d
printChan <parkchan59123129@gmail.com>
2026-06-21 22:32:15 +0900

    플레이어 움직임 구현

--- GoneTestPaper_Unity/Assets/02_Scripts/Player_Script/PlayerMove.cs
+++ GoneTestPaper_Unity/Assets/02_Scripts/Player_Script/PlayerMove.cs
@@ -1,16 +1,49 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMove : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    Rigidbody2D rigid;
+    public float moveSpeed = 5f;
+
+    Vector2 moveInput;
+
+    void Awake()
     {
-        
+        rigid = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        // immediateMove();
+        softMove();
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+// -----------------------------------------------------------------------------------------------
+
+    // 즉각적인 움직임 구현
+    void immediateMove()
+    {
+        rigid.linearVelocity = new Vector2(moveInput.x * moveSpeed, rigid.linearVelocity.y);
+    }
+
+    // 부드러운 움직임 구현
+    void softMove()
+    {
+        rigid.AddForce(new Vector2(moveInput.x * moveSpeed, 0f), ForceMode2D.Impulse);
+
+        if (rigid.linearVelocity.x > moveSpeed)
+        {
+            rigid.linearVelocity = new Vector2(moveSpeed, rigid.linearVelocity.y);
+        }
+        else if (rigid.linearVelocity.x < -moveSpeed)
+        {
+            rigid.linearVelocity = new Vector2(-moveSpeed, rigid.linearVelocity.y);
+        }
     }
 }
